9b3f30ad402240fb00c733942182046159080f7b
Don Mallory <dmallory@rabbet.com>
2021-03-10 16:06:37 -0600

    madness

--- src/App.js
+++ src/App.js
@@ -12,16 +12,23 @@ function App() {
 
   const [universalColor, setUniversalColor] = useState(null);
   const [time, setTime] = useState('');
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    setIsRunning(currentState => !currentState);
+  }
 
   useEffect(()=> {
+    if (!isRunning) return;
+    
     const timer = setInterval(()=> setTime(new Date().toLocaleTimeString()), 1000);
 
     return () => clearInterval(timer);
-  }, []);
+  }, [isRunning]);
 
   return(
     <div display="flex">
-      <Header resetColors={()=>setUniversalColor(null)} />
+      <Header toggle={()=>toggleTimer()} isRunning={isRunning} resetColors={()=>setUniversalColor(null)} />
       {tiles.map(index=><Tile setAll={setUniversalColor} key={index} color={universalColor || generateRandomColorCode()} time={time} />)}
     </div>
   )
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,10 @@
-const Header = ({resetColors}) => {
+const Header = ({isRunning, resetColors, toggle}) => {
     return (
         <div style={{height: 100, background: "lightblue"}}>
             <button onClick={resetColors}>
                 Set All Colors to Random
             </button>
+            <button onClick={toggle}>{isRunning ? "Please Fucking Stop" : "Start the Madness"}</button>
         </div>
     )
 }
